7cb0651a32fe96bf57401cbc4c06896eb36bb9df
laurenkim1 <laurenkim@college.harvard.edu>
2018-04-27 11:36:42 -0400

    app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, request
+
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST'])
+def result():
+    text = request.form['story'] # should display 'bar'
+    print text
+    return 'Received !' # response to your request.
+
+if __name__ == "__main__":
+    app.run()
